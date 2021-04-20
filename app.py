7686b576c39554994f8bfaf22307338bc7d01bf9
sebasalvarez13 <sebastian.alvarezarce@gmail.com>
2021-04-19 23:01:18 -0700

    Callback(): prints authorization url on console that redirects to callback.html and shows authorization code in url bar

--- app.py
+++ app.py
@@ -2,6 +2,8 @@
 
 from flask import Flask, render_template, request, redirect
 from main import CreatePlaylist
+from authorization import get_authorization, get_token
+from table_display import songdisplay
 
 app = Flask(__name__)
 
@@ -11,26 +13,37 @@ table = 'my_songs_played'
 
 @app.route("/", methods=['GET', 'POST'])
 def home():
-    return render_template('index.html')
+    return render_template('authorization.html')
+
+@app.route("/callback", methods=["GET", 'POST'])
+def callback():
+    print(get_authorization())
+    return render_template("callback.html") 
+
+@app.route("/getsongs", methods=["GET", 'POST'])
+def load_to_table():
+    code = request.form["code"] 
+    user_name = request.form["user_name"]
+    days_ago = request.form["days_ago"]
+
+    #pass code to obtain toke
+    response_json = get_token(code)
+    token = response_json["access_token"]
+    print(token)
+
+    #pass days_ago to spotify_pipeline_aws script
+    cp = CreatePlaylist(int(days_ago), token)
+    cp.load_to_table()
+
+    songdisplay(cp.get_spotify_songs())
+    
+    print("all modification done...")
+    return render_template('output.html')
 
 
 @app.route("/about", methods=["GET"])
 def about():
     return redirect("https://www.spotify.com/us/")
-
-
-@app.route("/getdays", methods=['POST'])
-def load_to_table():
-    name = request.form["name"]
-    user_name = request.form["user_name"]
-    days_ago = request.form["days_ago"]
-
-    #pass days_ago to spotify_pipeline_aws script
-    cp = CreatePlaylist(int(days_ago))
-    cp.load_to_table()
-
-    print("all modification done...")
-    return render_template('RecentPlayedSongsOutput.html', name = "Hey {}, here are your songs from {} days ago".format(name, days_ago))
 
 
 if __name__ == '__main__':
