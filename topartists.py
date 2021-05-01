e3c0344dc819cdb8dc7297f5b31681df54030835
Ubuntu <ubuntu@ip-172-31-2-147.us-west-2.compute.internal>
2021-05-01 20:44:55 +0000

    Fixed sql syntax

--- topartists.py
+++ topartists.py
@@ -4,22 +4,26 @@ import sqlite3
 import sqlalchemy
 import requests
 import pandas as pd
+import pymysql
+from pymysql import connections
+
+from aws_config import *
 
 def topartists():
-    DATABASE_LOCATION = "sqlite:///my_played_tracks.sqlite"
-    conn = sqlite3.connect('my_played_tracks.sqlite')
+    conn = connections.Connection(
+        host = custom_host,
+        port = 3306,
+        user = custom_user,
+        password = custom_pass,
+        db = custom_db
+    )
+
     cursor = conn.cursor()
 
-    sql_query = """
-    SELECT "artist_name",
-        COUNT("artist_name") AS "value_occurence"
-        FROM "my_played_tracks"
-        GROUP BY "artist_name"
-        ORDER BY "value_occurence" DESC
-        LIMIT 3;
+    sql_query = """SELECT artist_name, COUNT(*) AS 'occurence' FROM my_played_tracks GROUP BY artist_name ORDER BY occurence DESC LIMIT 5;"""
 
-    """
     cursor.execute(sql_query)
+
     print("Results fetched")
 
     results = pd.read_sql_query(sql_query, conn) 
