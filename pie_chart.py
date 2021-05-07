77aebfe30d5048baa69607b1cda21d20c9a06fa7
Ubuntu <ubuntu@ip-172-31-19-95.us-west-2.compute.internal>
2021-05-07 06:01:32 +0000

    Pass name and last name parameters to name chart.png

--- pie_chart.py
+++ pie_chart.py
@@ -7,19 +7,20 @@ import numpy as np
 
 from listening_times import filter_times
 
-def piechart(user_name):
-    songs_recurrence = filter_times()
-    #y = np.array(songs_recurrence)
-    labels = ["Commute", "Work", "Unwine", "Sleep"]
+def piechart(name, last_name):
+    songs_recurrence = filter_times(name, last_name)
+    labels = ["Commute", "Work", "Unwind", "Sleep"]
 
     fig = plt.figure(1, figsize=(6,6))
     ax = fig.add_subplot(111)
     colors=('b', 'g', 'r', 'y')
     ax.pie(songs_recurrence, colors = colors , labels = labels, autopct = '%1.1f%%', textprops = {'fontsize': 14})
-    plt.title("Times of most activity", fontsize = 22)
+    plt.title("Times of activity", fontsize = 22)
     plt.legend(["5-9am", "9-5pm", "5-10pm", "10-5am"])
 
-    plt.savefig("static/spotichart.png")
+    chart_name = "{}_{}_chart".format(name, last_name)
+    chart_name = chart_name.replace(" ", "")
+    plt.savefig("static/{}_{}chart.png".format(name, last_name))
 
 
 
