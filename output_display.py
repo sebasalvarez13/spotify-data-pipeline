322a84c7156218f63984ab9f7616c2be46949780
Ubuntu <ubuntu@ip-172-31-9-34.us-west-2.compute.internal>
2021-05-13 09:23:37 +0000

    Added .encode method to be able to display special characters from song/artist name in html file

--- output_display.py
+++ output_display.py
@@ -5,71 +5,51 @@ from IPython.display import HTML
 from topartists import topartists
 from recent_songs import recentsongs
 
-def output_display(df1, df2, name, last_name):
+#def output_display(df1, df2, name, last_name):
+def output_display(df1, df2):
     """Takes a dataframe as parameter and displays content as a responsive table in html page"""
-    
-    df1 = df1
-    df2 = df2
-    
+
     html1 = df1.to_html()
     html2 = df2.to_html()
 
-    output_file = "templates/output.html"
+    output_file = "/var/www/html/spotiSights/templates/output.html"
+    #chart_name = "{}_{}_chart.png".format(name, last_name)
 
-    try:
-        with open(output_file, 'w') as outputtable:
-            outputtable.write(
-            """
+    str1 = """
             <!DOCTYPE html>
-            <html>
+            <html lang="es">
             <head>
+            <meta charset="utf-8"/>
                 <link rel="stylesheet" href="/static/tables.css?v=1.1" type="text/css"></link>
                 <link rel="stylesheet" href="/static/images.css?v=1.1" type="text/css"></link>
                 <style>
                     tr:nth-child(even) {background-color: #f2f2f2;}
                 </style>
             </head>
-            
+
             <body>
                 <h2>Spotify Insights</h2>
-                <p>Take a look at your recent Spotify playing history, top listened artists and 
+                <p>Take a look at your recent Spotify playing history, top listened artists and
                 listening activity during your day.</p>
-                <div class = container1>
             """
-            )
-            outputtable.write(html1)
-            outputtable.write(
-            """
-                </div>
-            """
-            )
-            outputtable.write(
-            """
-                <div class = container2>
-            """    
-            )
-            outputtable.write(html2)
-            outputtable.write(
-            """
-                </div>
-            """
-            )
-            outputtable.write(
-            """      
-             <div class = img>
-                <img src="static/{}_{}_chart.png" alt="spotichart">
-             <\div>
 
-            </body>
-            </html>
-            """.format(name, last_name)
-            )
-            
-    except IOError as e:
+    try:
+        with open(output_file, "wb") as outputtable:
+            outputtable.write(str1.encode(encoding = "UTF-8",errors = "strict"))
+            outputtable.write("<div class = container1>\n".encode(encoding = "UTF-8",errors = "strict"))
+            outputtable.write(html1.encode(encoding = "UTF-8", errors = "strict"))
+            outputtable.write("</div>\n".encode(encoding = "UTF-8",errors = "strict"))
+            outputtable.write("<div class = container2>\n".encode(encoding = "UTF-8",errors = "strict"))
+            outputtable.write(html2.encode(encoding = "UTF-8",errors = "strict"))
+            outputtable.write("</div>\n".encode(encoding = "UTF-8",errors = "strict"))
+            #outputtable.write("<div class = img>")
+            #outputtable.write("<img src='static/{}' alt='spotichart'>".format(table_name)
+            #outputtable.write("<\div>")
+            outputtable.write("</body>\n</html>".encode(encoding = "UTF-8",errors = "strict"))
+
+    except Exception as e:
         print(e)
 
 
 if __name__ == "__main__":
-    df1 = recentsongs()
-    df2 = topartists()
-    table_display(df1, df2)
+    output_display()
