0695a6eec381dcf8b0756571736e47ce9469931e
Ubuntu <ubuntu@ip-172-31-9-34.us-west-2.compute.internal>
2021-05-13 09:34:04 +0000

    Replaced redirect_uri from localhost/callback to spoti-sights.com/callback

--- authorization.py
+++ authorization.py
@@ -7,11 +7,10 @@ from secrets import client_id, client_secret
 def get_authorization():
     query = "https://accounts.spotify.com/authorize"
     scope = "user-read-recently-played"
-    #redirect_uri = "https:%2F%2Fwww.builtinseattle.com%2F"
-    redirect_uri = "http://localhost/callback"
+    redirect_uri = "https://spoti-sights.com/callback"
     parameters = {
-        "client_id": client_id, 
-        "scope": scope, 
+        "client_id": client_id,
+        "scope": scope,
         "response_type": "code",
         "redirect_uri": redirect_uri
         }
@@ -19,11 +18,11 @@ def get_authorization():
 
     return(response.url)
 
-    
+
 def get_token(code):
     query = "https://accounts.spotify.com/api/token"
     scope = "user-read-recently-played"
-    redirect_uri = "http://localhost/callback"
+    redirect_uri = "https://spoti-sights.com/callback"
     grant_type = "authorization_code"
     code = code
 
@@ -39,9 +38,9 @@ def get_token(code):
     )
 
     response_json = response.json()
-    #print("This is the access token: " + response_json["access_token"])
+
     return(response_json)
-    
+
 
 if __name__ == "__main__":
     code = get_authorization()
