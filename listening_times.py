007631d4bbc0d66f44affcc4d4c8c95c2874b155
Ubuntu <ubuntu@ip-172-31-9-34.us-west-2.compute.internal>
2021-05-14 08:15:59 +0000

    Pass table name as parameter and added comments for code running status

--- listening_times.py
+++ listening_times.py
@@ -10,7 +10,7 @@ import pymysql
 from pymysql import connections
 from aws_config import *
 
-def listen_times(name, last_name):
+def listen_times(table_name):
     conn = connections.Connection(
         host = custom_host,
         port = 3306,
@@ -20,13 +20,10 @@ def listen_times(name, last_name):
     )
     cursor = conn.cursor()
 
-    table_name = "{}_{}_songs".format(name, last_name)
-    table_name = table_name.replace(" ", "")
-
     sql_query = "SELECT song_name, played_at, COUNT(*) AS 'occurence' FROM {} GROUP BY song_name ORDER BY 'occurence' DESC".format(table_name)
 
     cursor.execute(sql_query)
-    print("Results fetched")
+    print("Getting listening times for {}".format(table_name))
 
     results = pd.read_sql_query(sql_query, conn)
     times = results["played_at"]
@@ -37,8 +34,8 @@ def listen_times(name, last_name):
     return(times)
 
 
-def filter_times(name, last_name):
-    times = listen_times(name, last_name)
+def filter_times(table_name):
+    times = listen_times(table_name)
     commute = range(5,9)
     work = range(9,17)
     unwine = range(17,22)
@@ -48,10 +45,12 @@ def filter_times(name, last_name):
     unwine_list = []
     sleep_list = []
 
+    print("Filtering listening times for {}".format(table_name))
+
     for i in range(0,times.size):
         match = re.search("([0-9]*):", times[i])
         hour_of_day = int(match.group(1))
-        #print(hour_of_day)
+
         if hour_of_day in commute:
             commute_list.append(times[i])
         elif hour_of_day in work:
@@ -59,13 +58,9 @@ def filter_times(name, last_name):
         elif hour_of_day in unwine:
             unwine_list.append(times[i])
         else:
-            sleep_list.append(times[i])  
+            sleep_list.append(times[i])
 
     songs_recurrence = [len(commute_list), len(work_list), len(unwine_list), len(sleep_list)]
-    
+    print("Times filtered for {}".format(table_name))
     return(songs_recurrence)
 
-
-if __name__ == "__main__":
-    #print(listen_times())
-    filter_times()
